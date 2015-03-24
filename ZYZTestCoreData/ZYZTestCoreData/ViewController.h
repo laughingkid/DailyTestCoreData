e0f312caee3af3cc3b645a7b2a015a86130d8b3e
laughingkid <yzzhang.gw.con@163.com>
2015-03-24 15:20:40 +0800

    测试coredata
    1.了解coredata的属性
       插入,删除,修改
    2.了解coredata的性能
       插入速度,读取速度
    3.coredata能否作为2级缓存

---
+++ ZYZTestCoreData/ZYZTestCoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZYZTestCoreData
+//
+//  Created by yinzezhang on 15/3/24.
+//  Copyright (c) 2015年 yinzezhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
